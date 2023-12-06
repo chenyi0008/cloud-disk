5c4307fa53913ced91a78e6f9772b35c37546d52
chenyi <71563698@qq.com>
2023-12-06 18:43:23 +0800

    add:share basic detail module

--- core/internal/logic/share_basic_detail_logic.go
+++ core/internal/logic/share_basic_detail_logic.go
@@ -24,7 +24,17 @@ func NewShareBasicDetailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 }
 
 func (l *ShareBasicDetailLogic) ShareBasicDetail(req *types.ShareBasicDetailRequest) (resp *types.ShareBasicDetialReply, err error) {
-	// todo: add your logic here and delete this line
+	// 对分享记录的点击次数加1
+	_, err = l.svcCtx.Engine.Exec("update share_basic set click_num = click_num + 1 where identity = ?", req.Identity)
+	if err != nil {
+		return nil, err
+	}
+	// 获取资源的详细信息
+	resp = &types.ShareBasicDetialReply{}
+	l.svcCtx.Engine.Table("share_basic").
+		Select("share_basic.repository_identity, repository_pool.name, repository_pool.ext, repository_pool.size, repository_pool.path").
+		Join("LEFT", "repository_pool", "share_basic.repository_identity = repository_pool.identity").
+		Where("share_basic.identity = ?", req.Identity).Get(resp)
 
 	return
 }
